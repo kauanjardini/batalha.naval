0ecaaee5d4e0f4353be1279bfb34944cc0e7a68b
Adriel <adriel.fariasan@gmail.com>
2022-02-27 21:42:51 -0300

    Adiciona testes de posicionamento em pouco espaco

--- src/factories/tests/Gameboard.test.js
+++ src/factories/tests/Gameboard.test.js
@@ -14,3 +14,13 @@ test("O Gameboard posiciona uma embarcacao na vertical", () => {
   const s = { length: 4 };
   expect(g.addShip(s, [0, 0], true)).toBe(true);
 });
+
+test("O Gameboard emite erro quando embarcacao excede espaco na horizontal", () => {
+  const s = { length: 4 };
+  expect(() => g.addShip(s, [8, 0], false)).toThrow();
+});
+
+test("O Gameboard emite erro quando embarcacao excede espaco na vertical", () => {
+  const s = { length: 4 };
+  expect(() => g.addShip(s, [0, 8], true)).toThrow();
+});
