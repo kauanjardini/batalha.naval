f50ea5fd3aa8c38ec8a42b582de20373b907332f
Adriel <adriel.fariasan@gmail.com>
2022-02-27 21:41:47 -0300

    Adiciona testes de posicionamento de embarcacao

--- src/factories/tests/Gameboard.test.js
+++ src/factories/tests/Gameboard.test.js
@@ -1,1 +1,16 @@
 import Gameboard from "../Gameboard";
+
+let g;
+beforeEach(() => {
+  g = Gameboard();
+});
+
+test("O Gameboard posiciona uma embarcacao na horizontal", () => {
+  const s = { length: 4 };
+  expect(g.addShip(s, [0, 0], false)).toBe(true);
+});
+
+test("O Gameboard posiciona uma embarcacao na vertical", () => {
+  const s = { length: 4 };
+  expect(g.addShip(s, [0, 0], true)).toBe(true);
+});
