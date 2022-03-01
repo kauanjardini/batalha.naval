fa94c2693bc2bf5f432ec3da03368873b7d9b48b
Adriel <adriel.fariasan@gmail.com>
2022-02-28 23:56:08 -0300

    Implementa factory de Player

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -1,8 +1,45 @@
 function Player(name = "Player", computer = false) {
-  if (computer) {
-    return { name };
+  let type;
+  const playerObj = {};
+  const usedCoordinates = [];
+
+  function randCoordinates() {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    return [x, y];
   }
-  return { name };
+
+  // terponsible for checking if pair of coordinates are equal
+  function equalCoors(a, b) {
+    if (a[0] === b[0] && a[1] === b[1]) {
+      return true;
+    }
+    return false;
+  }
+
+  let coors = randCoordinates();
+  function makeMove() {
+    // eslint-disable-next-line no-loop-func
+    while (usedCoordinates.some((c) => equalCoors(c, coors))) {
+      coors = randCoordinates();
+    }
+    usedCoordinates.push(coors);
+    return coors;
+  }
+
+  if (computer) {
+    type = "code";
+    playerObj.makeMove = makeMove;
+  } else {
+    type = "human";
+  }
+
+  playerObj.name = name;
+  playerObj.randCoordinates = randCoordinates;
+  Object.defineProperties(playerObj, {
+    type: { get: () => type },
+  });
+  return playerObj;
 }
 
 export default Player;
