ffaad7bbd643d1f09805fdddaea359063b2801d5
Adriel <adriel.fariasan@gmail.com>
2022-02-28 22:53:41 -0300

    Adiciona teste de existencia da funcao makeMove

--- src/factories/tests/Player.test.js
+++ src/factories/tests/Player.test.js
@@ -3,3 +3,11 @@ import Player from "../Player";
 test("Jogador criado possui nome", () => {
   expect(Player("fulano", false).name).toBe("fulano");
 });
+
+test("Jogador tipo PC possui funcao makeMove", () => {
+  expect(Player("pc", true).makeMove).not.toBe(undefined);
+});
+
+test("Jogador tipo pessoa nao possui funcao makeMove", () => {
+  expect(Player("fulano", false).makeMove).toBe(undefined);
+});
