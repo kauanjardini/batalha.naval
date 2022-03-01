3c4dfa93215d00bee471d31473e293506abf74b9
Adriel <adriel.fariasan@gmail.com>
2022-03-01 12:38:38 -0300

    Implementa movimento considerando jogada anterior

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -3,9 +3,13 @@ function Player(name = "Player", computer = false) {
   const playerObj = {};
   const usedCoordinates = [];
 
+  function randNumber(min = 0, max = 9) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   function randCoordinates() {
-    const x = Math.floor(Math.random() * 10);
-    const y = Math.floor(Math.random() * 10);
+    const x = randNumber();
+    const y = randNumber();
     return [x, y];
   }
 
@@ -17,8 +21,68 @@ function Player(name = "Player", computer = false) {
     return false;
   }
 
+  function inRange(x, range = [0, 9]) {
+    if (x >= range[0] && x <= range[1]) {
+      return true;
+    }
+    return false;
+  }
+
   let coors = randCoordinates();
-  function makeMove() {
+  function makeMove(lastWasHit = false, triedX = false, triedY = false) {
+    if (lastWasHit) {
+      // determine if it will shoot on x or y axis
+      let xAxis;
+      if (triedX) {
+        xAxis = 0;
+      } else if (triedY) {
+        xAxis = 1;
+      } else {
+        xAxis = randNumber(0, 1);
+      }
+
+      // last coordinates which were a hit
+      const lastCoors = usedCoordinates[usedCoordinates.length - 1];
+      let x = lastCoors[0];
+      let y = lastCoors[1];
+
+      // determine if it will first try to add or subtract
+      const add = randNumber(0, 1);
+
+      // y changes, x stays the same
+      if (xAxis === 1) {
+        y += add === 1 ? 1 : -1;
+        if (!inRange(y) || usedCoordinates.some((c) => equalCoors(c, [x, y]))) {
+          y += add === 0 ? 2 : -2;
+          if (
+            !inRange(y) ||
+            usedCoordinates.some((c) => equalCoors(c, [x, y]))
+          ) {
+            if (triedY) {
+              return makeMove();
+            }
+            return makeMove(true, true, false);
+          }
+          return [x, y];
+        }
+        return [x, y];
+      }
+
+      // x changes, y stays the same
+      x += add === 1 ? 1 : -1;
+      if (!inRange(x) || usedCoordinates.some((c) => equalCoors(c, [x, y]))) {
+        x += add === 0 ? 2 : -2;
+        if (!inRange(x) || usedCoordinates.some((c) => equalCoors(c, [x, y]))) {
+          if (triedX) {
+            return makeMove();
+          }
+          return makeMove(true, false, true);
+        }
+        return [x, y];
+      }
+      return [x, y];
+    }
+
     // eslint-disable-next-line no-loop-func
     while (usedCoordinates.some((c) => equalCoors(c, coors))) {
       coors = randCoordinates();
