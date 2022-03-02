d6fc282eb6ca20333ed1773cdcf288f8c7e483a6
Adriel <adriel.fariasan@gmail.com>
2022-03-02 17:29:03 -0300

    Adiciona chamada de telaInicio ao carregar site

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Footer from "./ui/footer";
 import "./index.scss";
+import mostrarTela from "./utilidades/mostrarTela";
+import telaInicio from "./ui/telaInicio";
+
+mostrarTela(telaInicio());
 
 Footer.carregarFooter();
