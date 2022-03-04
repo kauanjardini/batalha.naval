07c87c7237de8e64cc87a6527ff4f5082e533552
Adriel <adriel.fariasan@gmail.com>
2022-03-04 20:07:01 -0300

    Implementa funcao para mostrar jogadas

--- src/ui/tabuleiroHtml.js
+++ src/ui/tabuleiroHtml.js
@@ -1,6 +1,26 @@
 import novoElemento from "../utilidades/novoElemento";
 
-function tabuleiroHtml(tabId) {
+function conteudoDiv(board, i, j, tabId) {
+  let div;
+
+  if (board[i][j].attacked) {
+    if (board[i][j].hit) {
+      div = novoElemento("div", "", ["acerto"]);
+      div.innerText = board[i][j].ship.length;
+    } else {
+      const icone = novoElemento("i", "", ["fa-solid", "fa-bomb"]);
+
+      div = novoElemento("div", "", ["erro"]);
+      div.appendChild(icone);
+    }
+  } else {
+    div = novoElemento("div", `${tabId}-${i}-${j}`);
+  }
+
+  return div;
+}
+
+function tabuleiroHtml(tabId, board = []) {
   const linhas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
   const tabuleiro = novoElemento("div", "", ["tabuleiro"]);
 
@@ -18,7 +38,13 @@ function tabuleiroHtml(tabId) {
     tabuleiro.appendChild(p);
 
     for (let j = 0; j < 10; j += 1) {
-      const div = novoElemento("div", `${tabId}-${i}-${j}`);
+      let div;
+
+      if (board.length !== 0) {
+        div = conteudoDiv(board, i, j, tabId);
+      } else {
+        div = novoElemento("div", `${tabId}-${i}-${j}`);
+      }
       tabuleiro.appendChild(div);
     }
   }
