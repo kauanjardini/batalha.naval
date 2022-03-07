88b90831070bef595e63f3d22ad6ee05646b77da
Adriel <adriel.fariasan@gmail.com>
2022-03-07 16:18:56 -0300

    Implementa fim de partida

--- src/ui/telaJogar.js
+++ src/ui/telaJogar.js
@@ -4,6 +4,10 @@ import "./telaJogar.scss";
 import telaProxJogador from "./telaProxJogador";
 import mostrarTela from "../utilidades/mostrarTela";
 import disparo from "../audio/disparo.mp3";
+import comemoracao from "../audio/comemoracao.mp3";
+import triste from "../audio/triste.mp3";
+// eslint-disable-next-line import/no-cycle
+import telaVencedor from "./telaVencedor";
 
 function telaJogar(jogadores, tabuleiros, foiAcerto = false) {
   let jaJogou = false;
@@ -26,6 +30,24 @@ function telaJogar(jogadores, tabuleiros, foiAcerto = false) {
 
   const btnContinuar = novoElemento("button");
   btnContinuar.innerText = "Continuar";
+
+  const h2 = novoElemento("h2");
+  h2.innerText = `Faça uma jogada, ${jogadores[0].name}`;
+
+  div.appendChild(h2);
+  const p1 = novoElemento("p");
+  p1.innerText = "Atire no seu oponente:";
+
+  const p2 = novoElemento("p");
+  p2.innerText = "A situação do seu tabuleiro:";
+
+  const divTab1 = novoElemento("div", "", ["div-tab"]);
+  divTab1.appendChild(p1);
+  divTab1.appendChild(tabOponente);
+
+  const divTab2 = novoElemento("div", "", ["div-tab"]);
+  divTab2.appendChild(p2);
+  divTab2.appendChild(tabPessoal);
 
   tabOponente.addEventListener("click", (e) => {
     const id = e.target.id.split("-");
@@ -52,13 +74,37 @@ function telaJogar(jogadores, tabuleiros, foiAcerto = false) {
         e.target.appendChild(icone);
       }
 
-      // verificar se o jogo acabou
+      // verifica se jogador ganhou
+      if (tabuleiros[1].allShipsSunked()) {
+        div.appendChild(telaVencedor(jogadores[0].name));
 
-      if (jogadores[1].type === "code") {
+        divTab2.removeChild(h2);
+        divTab2.appendChild(
+          tabuleiroHtml("tabOponente", tabuleiros[0].board())
+        );
+
+        const audioComemoracao = new Audio(comemoracao);
+        audioComemoracao.play();
+      } else if (jogadores[1].type === "code") {
         const jogada = jogadores[1].makeMove(foiAcerto, false, false);
         const atacado = tabuleiros[0].receiveAttack(jogada);
+
         if (atacado === "hit") {
-          proxTela(true, true);
+          // veroifica se pc ganhou
+          if (tabuleiros[0].allShipsSunked()) {
+            div.removeChild(h2);
+            div.appendChild(telaVencedor(jogadores[1].name));
+
+            divTab2.removeChild(divTab2.childNodes[1]);
+            divTab2.appendChild(
+              tabuleiroHtml("tabOponente", tabuleiros[0].board())
+            );
+
+            const audioTriste = new Audio(triste);
+            audioTriste.play();
+          } else {
+            proxTela(true, true);
+          }
         } else {
           proxTela(true, false);
         }
@@ -68,24 +114,6 @@ function telaJogar(jogadores, tabuleiros, foiAcerto = false) {
       }
     }
   });
-
-  const h2 = novoElemento("h2");
-  h2.innerText = `Faça uma jogada, ${jogadores[0].name}`;
-
-  div.appendChild(h2);
-  const p1 = novoElemento("p");
-  p1.innerText = "Atire no seu oponente:";
-
-  const p2 = novoElemento("p");
-  p2.innerText = "A situação do seu tabuleiro:";
-
-  const divTab1 = novoElemento("div", "", ["div-tab"]);
-  divTab1.appendChild(p1);
-  divTab1.appendChild(tabOponente);
-
-  const divTab2 = novoElemento("div", "", ["div-tab"]);
-  divTab2.appendChild(p2);
-  divTab2.appendChild(tabPessoal);
 
   section.appendChild(div);
   section.appendChild(divTab1);
