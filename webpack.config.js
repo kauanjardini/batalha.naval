10f8d096f5d206df45d6d8515cca14da69d752d6
kauan jardini <82685000+kauanjardini@users.noreply.github.com>
2023-11-23 21:01:00 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+
+
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
